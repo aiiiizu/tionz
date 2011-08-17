98b7b33fdc983cef262e639c724e54e09690469d
daisuke <macchan5.19@gmail.com>
2011-08-17 19:51:42 +0900

    Fix battery temperature and service name.

--- src/com/aiiiizu/tionz/BatteryReceiver.java
+++ src/com/aiiiizu/tionz/BatteryReceiver.java
@@ -14,14 +14,14 @@ public class BatteryReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		// バッテリー情報が変化した際の情報表示
 		if (intent.getAction().equals(Intent.ACTION_BATTERY_CHANGED)) {
-			Log.d("TionzWidget", "バッテリ温度 : " + String.valueOf(intent.getIntExtra("temperature", 0)));
+			Log.d("TionzWidget", "battery temp:" + String.valueOf(intent.getIntExtra("temperature", 0)));
 
 			int temperature = intent.getIntExtra("temperature", 0);
 
 			AppWidgetManager appWidget = AppWidgetManager.getInstance(context);
 			ComponentName name = new ComponentName(context, MainActivity.class);
 			RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.main);
-			remoteView.setTextViewText(R.id.temp_id, String.valueOf(temperature));
+			remoteView.setTextViewText(R.id.temp_id, String.valueOf(temperature / 10.0));
 			appWidget.updateAppWidget(name, remoteView);
 		}
 	}
