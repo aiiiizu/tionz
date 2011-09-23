061cce74d40351ad545d248e85f64fdfaefa2102
aiiiizu <aiiiizu@gmail.com>
2011-09-23 10:23:59 +0900

    ヘルプページ編集

--- gen/com/aiiiizu/tionz/R.java
+++ gen/com/aiiiizu/tionz/R.java
@@ -19,13 +19,16 @@ public final class R {
         public static final int facebook_icon=0x7f020001;
         public static final int icon=0x7f020002;
         public static final int twitter=0x7f020003;
-        public static final int widget_background=0x7f020004;
+        public static final int widget_1=0x7f020004;
+        public static final int widget_2=0x7f020005;
+        public static final int widget_background=0x7f020006;
     }
     public static final class id {
-        public static final int like_btn=0x7f070003;
-        public static final int login=0x7f070000;
-        public static final int temp_id=0x7f070002;
-        public static final int tweet_btn=0x7f070001;
+        public static final int ScrollView=0x7f070000;
+        public static final int like_btn=0x7f070004;
+        public static final int login=0x7f070001;
+        public static final int temp_id=0x7f070003;
+        public static final int tweet_btn=0x7f070002;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -33,7 +36,7 @@ public final class R {
         public static final int widget=0x7f030002;
     }
     public static final class string {
-        public static final int about_abstract=0x7f060013;
+        public static final int about_abstract=0x7f060010;
         public static final int about_title=0x7f06000e;
         /**  about information 
          */
@@ -42,13 +45,16 @@ public final class R {
         /**  system information 
          */
         public static final int app_name=0x7f060000;
-        public static final int change_abstract=0x7f060014;
-        public static final int change_title_header=0x7f060010;
+        public static final int change_abstract_1=0x7f060012;
+        public static final int change_title_header=0x7f060011;
         /**  widget information 
          */
         public static final int header=0x7f060001;
-        public static final int notice_abstract=0x7f060015;
-        public static final int notice_title_header=0x7f060011;
+        public static final int notice_abstract_1=0x7f060014;
+        public static final int notice_abstract_2=0x7f060015;
+        public static final int notice_abstract_3=0x7f060016;
+        public static final int notice_abstract_4=0x7f060017;
+        public static final int notice_title_header=0x7f060013;
         public static final int setting_account_facebook=0x7f060008;
         public static final int setting_account_facebook_dialogtitle=0x7f060009;
         public static final int setting_account_facebook_label=0x7f060007;
@@ -62,8 +68,10 @@ public final class R {
         public static final int share_contents_dialogtitle=0x7f06000c;
         public static final int share_contents_label=0x7f06000a;
         public static final int temperature=0x7f060002;
-        public static final int use_abstract=0x7f060016;
-        public static final int use_title_header=0x7f060012;
+        public static final int use_abstract_1=0x7f06001a;
+        public static final int use_abstract_2=0x7f06001b;
+        public static final int use_sub_title_header_1=0x7f060019;
+        public static final int use_title_header=0x7f060018;
     }
     public static final class xml {
         public static final int setting=0x7f040000;
